0f32031aadb79e8e53a877a1030f6ef374780e76
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-06-09 15:32:44 -0700

    Add function playRound()
    This function takes in two(2) parameters
    playerSelection and computerSelection. With this
    two input, it will determine who won the
    rock-paper-scissors.

--- javascript.js
+++ javascript.js
@@ -17,3 +17,25 @@ function getComputerChoice() {
 function randomIntegerGenerator(maxInt = "3") {
     return Math.floor(Math.random() * maxInt);
 }
+
+function playRound(playerSelection = "", computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "Draw!! You both pick " + playerSelection;
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors" || 
+    playerSelection === "paper" && computerSelection === "rock" || 
+    playerSelection === "scissors" && computerSelection === "paper") {
+        return "Win!! " + playerSelection + " beats " + computerSelection + "!!";
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper" || 
+    playerSelection === "paper" && computerSelection === "scissors" || 
+    playerSelection === "scissors" && computerSelection === "rock") {
+        return "Lose " + computerSelection + " beats " + playerSelection + "!!";
+    }
+    else 
+        return "Invalid Input. Please only use: Rock, Paper, Scissors. NOT case-sensitive";
+}
