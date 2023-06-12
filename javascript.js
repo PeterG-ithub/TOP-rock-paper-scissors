ff420f5b76bcaae941c0ae420b6a01d844c48009
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-06-12 11:16:17 -0700

    Add functionality to the buttons. Add Scoreboard
    Added functionality to buttons so that clicking
    on them will make use the function playRound().
    Added a scoreboard which can be updated with
    function update(). Styled the scoreboard to be
    consistent with the page.

--- javascript.js
+++ javascript.js
@@ -1,70 +1,104 @@
+const gameOutcome = document.querySelector(".game-outcome")
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+
+btnRock.addEventListener('click', () => playRound('rock'));
+btnPaper.addEventListener('click', () => playRound('paper'));
+btnScissors.addEventListener('click', () => playRound('scissors'));
+
+
 function getComputerChoice() {
     switch(randomIntegerGenerator(3)) {
     case 0:
-        return "Rock";
+        return "rock";
         break;
     case 1:
-        return "Paper";
+        return "paper";
         break;
     case 2:
-        return "Scissors";
+        return "scissors";
         break;
     default:
         return "";
     }
 }
-
 function randomIntegerGenerator(maxInt = "3") {
     return Math.floor(Math.random() * maxInt);
 }
 
 function playRound(playerSelection = "", computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+    //playerSelection = playerSelection.toLowerCase();      no longer needed
     computerSelection = getComputerChoice();
-    computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
+        update("draw"); 
         return "Draw!! You both pick " + playerSelection;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors" || 
     playerSelection === "paper" && computerSelection === "rock" || 
     playerSelection === "scissors" && computerSelection === "paper") {
+        update("win");
         return "Win!! " + playerSelection + " beats " + computerSelection + "!!";
     }
     else if (playerSelection === "rock" && computerSelection === "paper" || 
     playerSelection === "paper" && computerSelection === "scissors" || 
     playerSelection === "scissors" && computerSelection === "rock") {
+        update("lose");
         return "Lose " + computerSelection + " beats " + playerSelection + "!!";
     }
     else 
         return "Invalid Input. Please only use: Rock, Paper, Scissors. NOT case-sensitive";
 }
 
-function game() {
-    let input;
-    let playerScore = 0;
-    let compScore = 0;
-    for (let i = 1; i <= 5; i++) {
-        input = prompt("Rock-Paper-Scissors! Pick your poison: ", "rock");
-        let temp = playRound(input);
-        switch(temp.charAt(0)) {
-            case 'L':
-                compScore++;
-                break;
-            case 'W':
-                playerScore++;
-                break;
-            default:
-                break;
-        }
-        console.log(temp);
+
+//Update text and Score
+function update(outcome = "") {
+    switch (outcome) {
+        case "draw":
+            gameOutcome.textContent = "Draw!!";
+            gameOutcome.style.color = "yellow";
+            break;
+        case "win":
+            gameOutcome.textContent = "Win!";
+            gameOutcome.style.color = "green";
+            break;
+        case "lose":
+            gameOutcome.textContent = "Lose!";
+            gameOutcome.style.color = "red";
+            break;
+        default:
+            gameOutcome.textContent = "";
     }
-    if (playerScore > compScore) {
-        console.log("Player Wins!! with a score of " + playerScore);
-    }
-    else if (playerScore < compScore) {
-        console.log("Computer Wins!! with a score of " + compScore);
-    }
-    else 
-        console.log("Draw!!!!!");
+    
 }
+
+//~~~~~~~~~~~~Console Version~~~~~~~~~~~~//
+// function game() {
+//     let input;
+//     let playerScore = 0;
+//     let compScore = 0;
+//     for (let i = 1; i <= 5; i++) {
+//         input = prompt("Rock-Paper-Scissors! Pick your poison: ", "rock");
+//         let temp = playRound(input);
+//         switch(temp.charAt(0)) {
+//             case 'L':
+//                 compScore++;
+//                 break;
+//             case 'W':
+//                 playerScore++;
+//                 break;
+//             default:
+//                 break;
+//         }
+//         console.log(temp);
+//     }
+//     if (playerScore > compScore) {
+//         console.log("Player Wins!! with a score of " + playerScore);
+//     }
+//     else if (playerScore < compScore) {
+//         console.log("Computer Wins!! with a score of " + compScore);
+//     }
+//     else 
+//         console.log("Draw!!!!!");
+// }
